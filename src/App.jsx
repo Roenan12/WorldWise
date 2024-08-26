a4e5e1583f4140355bbce6ff65fd82ddafa08522
Roenan Halili <haliliroenan12@gmail.com>
2024-08-26 16:20:08 +0800

    Add css module and starter files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
