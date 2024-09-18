66770a9c36c07fe9b5d94dfb206cecadf637e6af
Roenan Halili <haliliroenan12@gmail.com>
2024-09-18 23:36:37 +0800

    refactor: configure serverless function and update json-server version

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(cors()); 
+server.use(cors());
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.bodyParser);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.use("/api", router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
